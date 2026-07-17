713b9acef2ba7223122a02df096359a0259db8ca
Md. Kowshik <168761417+SadBIOS@users.noreply.github.com>
2026-07-17 17:09:41 +0600

    Add files via upload

--- runtime/build_engine.sh
+++ runtime/build_engine.sh
@@ -99,12 +99,15 @@ function extract_exact() {
         *.tar.xz)
             tar -xvJf "$archive" -C "$BUILD_DIR"
         ;;
+
         *.tar.bz2)
             tar -xvjf "$archive" -C "$BUILD_DIR"
         ;;
+
         *.tar.gz|*.tgz)
             tar -xvzf "$archive" -C "$BUILD_DIR"
         ;;
+        
     esac
 }
 
